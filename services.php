77ab4f7a55a811d96a0fee4725cb5e5844de05cd
fhasanli <fhesenli92@gmail.com>
2019-09-02 12:29:38 +0400

    Changes

--- services.php
+++ services.php
@@ -37,7 +37,7 @@ require_once "ermanager/pages/includes/config.php";
             <div class="auto-container">
                 <div class="inner-container clearfix">
                     <ul class="bread-crumb pull-left">
-                        <li><a href="index.html">Ana Səhifə</a></li>
+                        <li><a href="<?=SITE_PATH?>">Ana Səhifə</a></li>
                         <li>Xidmətlərimiz</li>
                     </ul>
                 </div>
@@ -56,10 +56,10 @@ require_once "ermanager/pages/includes/config.php";
                     <div class="services-block col-md-4 col-sm-6 col-xs-12">
                         <div class="inner-box">
                             <div class="image">
-                                <a href="hvac.html"><img src="images/resource/service-1.jpg" alt="" /></a>
+                                <a href="<?=SITE_PATH?>/hvac"><img src="images/resource/service-1.jpg" alt="" /></a>
                             </div>
                             <div class="lower-content">
-                                <h3><a href="hvac.html">HVAC Sistemləri</a></h3>
+                                <h3><a href="<?=SITE_PATH?>/hvac">HVAC Sistemləri</a></h3>
                             </div>
                         </div>
                     </div>
@@ -68,10 +68,10 @@ require_once "ermanager/pages/includes/config.php";
                     <div class="services-block col-md-4 col-sm-6 col-xs-12">
                         <div class="inner-box">
                             <div class="image">
-                                <a href="fire-protection.html"><img src="images/resource/service-2.jpg" alt="" /></a>
+                                <a href="<?=SITE_PATH?>/fire-protection"><img src="images/resource/service-2.jpg" alt="" /></a>
                             </div>
                             <div class="lower-content">
-                                <h3><a href="fire-protection.html">Yanğından Mühafizə</a></h3>
+                                <h3><a href="<?=SITE_PATH?>/fire-protection">Yanğından Mühafizə</a></h3>
                             </div>
                         </div>
                     </div>
@@ -80,10 +80,10 @@ require_once "ermanager/pages/includes/config.php";
                     <div class="services-block col-md-4 col-sm-6 col-xs-12">
                         <div class="inner-box">
                             <div class="image">
-                                <a href="management.html"><img src="images/resource/service-3.jpg" alt="" /></a>
+                                <a href="<?=SITE_PATH?>/management"><img src="images/resource/service-3.jpg" alt="" /></a>
                             </div>
                             <div class="lower-content">
-                                <h3><a href="management.html">Layihələndirmə</a></h3>
+                                <h3><a href="<?=SITE_PATH?>/management">Layihələndirmə</a></h3>
                             </div>
                         </div>
                     </div>
@@ -91,10 +91,10 @@ require_once "ermanager/pages/includes/config.php";
                     <div class="services-block col-md-4 col-sm-6 col-xs-12">
                         <div class="inner-box">
                             <div class="image">
-                                <a href="montaj.html"><img src="images/resource/service-3.jpg" alt="" /></a>
+                                <a href="<?=SITE_PATH?>/montaj"><img src="images/resource/service-3.jpg" alt="" /></a>
                             </div>
                             <div class="lower-content">
-                                <h3><a href="montaj.html">Montaj</a></h3>
+                                <h3><a href="<?=SITE_PATH?>/montaj">Montaj</a></h3>
                             </div>
                         </div>
                     </div>
@@ -106,99 +106,9 @@ require_once "ermanager/pages/includes/config.php";
         <!--End Services Page Section-->
 
         <!--Main Footer-->
-        <footer class="main-footer">
-            <div class="auto-container">
-                <!--Widgets Section-->
-                <div class="widgets-section">
-                    <div class="row clearfix">
-
-                        <!--big column-->
-                        <div class="big-column col-md-6 col-sm-12 col-xs-12">
-                            <div class="row clearfix">
-
-                                <!--Footer Column-->
-                                <div class="footer-column col-md-7 col-sm-6 col-xs-12">
-                                    <div class="footer-widget logo-widget">
-                                        <div class="logo">
-                                            <a href="index.html"><img src="images/logo_eco_footer.png" alt="" /></a>
-                                        </div>
-                                        <div class="text">8 ildən artıq müddətdə fəaliyyetimizin bizə qazandırdığı
-                                            təcrübə, sahəsinin peşəkarı olan işçi kollektivimiz və gördüyümüz işlərin
-                                            keyfiyyətinə zəmanətlə sizin xidmətinizdəyik.</div>
-                                    </div>
-                                </div>
-
-                                <!--Footer Column-->
-                                <div class="footer-column col-md-5 col-sm-6 col-xs-12">
-                                    <div class="footer-widget links-widget">
-                                        <h2>Quick Links</h2>
-                                        <div class="widget-content">
-                                            <ul class="list">
-                                                <li><a href="#">Home</a></li>
-                                                <li><a href="#">About Us</a></li>
-                                                <li><a href="#">Testimonials</a></li>
-                                                <li><a href="#">Request Call Back</a></li>
-                                                <li><a href="#">Contact Us</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!--big column-->
-                        <div class="big-column col-md-6 col-sm-12 col-xs-12">
-                            <div class="row clearfix">
-
-                                <!--Footer Column-->
-                                <div class="footer-column col-md-6 col-sm-6 col-xs-12">
-                                    <div class="footer-widget links-widget">
-                                        <h2>Xidmətlərimiz</h2>
-                                        <div class="widget-content">
-                                            <ul class="list">
-                                                <li><a href="#">HVAC Sistemləri</a></li>
-                                                <li><a href="#">Yanğından Mühafizə</a></li>
-                                                <li><a href="#">Layihələndirmə</a></li>
-                                                <li><a href="#">Montaj</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!--Footer Column-->
-                                <div class="footer-column col-md-6 col-sm-6 col-xs-12">
-                                    <div class="footer-widget info-widget">
-                                        <h2>Əlaqə</h2>
-                                        <div class="widget-content">
-                                            <div class="number">(99455) 123 45 67</div>
-                                            <div class="text">56, Yusif Seferov, Azerbaijan ecosphere@contact.gmail.com
-                                            </div>
-                                            <ul class="social-icon-one">
-                                                <li><a href="#"><span class="fa fa-facebook"></span></a></li>
-                                                <li><a href="#"><span class="fa fa-linkedin"></span></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <!--Footer Bottom-->
-                <div class="footer-bottom">
-                    <div class="clearfix">
-                        <div class="centered">
-                            <div class="copyright">&copy; Copyright Ecosphere 2019. All right reserved.</div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </footer>
+        <?php
+        require_once "layouts/footer.php";
+        ?>
 
     </div>
     <!--End pagewrapper-->
